419e933dbcd458fdc3a22aec2695f4338613deb7
nelsonJM <jmarknel@gmail.com>
2014-02-13 09:02:42 -0600

    Removed Object.create code

--- slider.js
+++ slider.js
@@ -1,11 +1,3 @@
-if(typeof Object.create !== 'function'){
-	Object.create = function(obj){
-		function F(){}
-	F.prototype = obj;
-		return new F();
-	};
-}
-
 (function($, window, document, undefined){
 	var Gallery = {
 		init: function(options, elem){
